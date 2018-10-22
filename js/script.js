1937373a8926269b2542bf18266a147415701569
Roman Y <roman4wm@gmail.com>
2018-10-22 15:00:11 +0300

    script

--- js/script.js
+++ js/script.js
@@ -4,26 +4,41 @@ let userCode = document.getElementById('userCode');
 let userCodeButton = document.getElementById('userCodeButton');
 
 // Message
+let notEmpty = 'Введите четыре числа без пробелов.';
+let onlyFour = 'Введите только четыре числа, которые не повторялись.';
 
-let notEmpty = 'Ввудите четыре числа без пробелов.';
+// Проверить, не повторяються ли числа
+let notRepeatingNumbers = function (num) {
+    if (num.length != 4) {
+        alert(onlyFour);
+    }
+    let n = 0;
+    for (let i = 0; i < 4; i++) {
+        delete num[i];
+        if (num.includes(num[i])){
+            console.log(num);
+        }
+        console.log('ok');
+    }
+};
 
 userCodeButton.onclick = function() {
 
     let userCodeSet = userCode.value.trim();
 
-    if (!userCodeSet) {
+    if (!userCodeSet || isNaN(userCodeSet)) {
         alert(notEmpty);
     }
 
+    notRepeatingNumbers(userCodeSet);
 
-
-    userCode.value += ' ->в onclick ';
-
-    userCode.focus(); // вызов инициирует событие onfocus
-
-    userCode.value += ' из onclick-> ';
+    // userCode.value += ' ->в onclick ';
+    //
+    // userCode.focus(); // вызов инициирует событие onfocus
+    //
+    // userCode.value += ' из onclick-> ';
 };
 
-userCode.onfocus = function() {
-    userCode.value += ' !focus! ';
-};
+// userCode.onfocus = function() {
+//     userCode.value += ' !focus! ';
+// };
